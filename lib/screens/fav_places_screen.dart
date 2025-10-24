ce1d67ea58c86f025c6984945a8d28f4988e9b7b
Mihai Dragusin <33466422+MNDragusin@users.noreply.github.com>
2025-10-24 13:17:08 +0200

    initial setup

---
+++ lib/screens/fav_places_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class FavPlacesScreen extends StatelessWidget {
+  const FavPlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      appBar: AppBar(title: Text('Favorite Places')),
+      body: Center(child: Text('This is the Favorite Places Screen')),
+    );
+  }
+}
